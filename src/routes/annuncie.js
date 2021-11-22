3336fc86f88db50021c688d2b0df0241965e3647
Jaiber Flor <jaiver.navia@gmail.com>
2021-11-22 15:38:25 -0500

    prueba del back

---
+++ src/routes/annuncie.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const controllerAnnuncie = require("../controllers/annuncie");
+
+router.post("/new-announcie", controllerAnnuncie.create);
+router.get("/get-announcies", controllerAnnuncie.getAnnuncies);
+router.patch("/update-announcie", controllerAnnuncie.updateAnnuncie);
+
+module.exports = router;
